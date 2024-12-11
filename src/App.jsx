ab1b4bd69a18d8843662dfce7ff444c488be8caf
Rohan-Shah-312003 <rohan312003@gmail.com>
2024-12-12 04:55:19 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+const App = () => {
+    return (
+        <div>
+            <section className="h-screen bg-green-400">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.
+                </p>
+            </section>
+        </div>
+    )
+}
+export default App
